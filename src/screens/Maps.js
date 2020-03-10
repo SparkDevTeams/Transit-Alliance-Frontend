f843cbf6b00607913ad7916e2f8e168893a45401
Fulton Garcia <fulton@geniusplaza.com>
2020-03-10 18:28:31 -0400

    redirect for Maps

--- src/screens/Maps.js
+++ src/screens/Maps.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Map, Marker, Popup, TileLayer, Polyline} from 'react-leaflet'
 import styled from 'styled-components';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Redirect } from 'react-router-dom';
 import "../api/api";
 
 class Maps extends React.Component {
     render(){
+        if(!this.props.location.state){
+            return <Redirect to="/"></Redirect>
+        }
         
         return<Map center={[25.8,-80.3]} zoom={12}>
             <Polyline  positions={this.props.location.state.completeInfo}  />
