cb8a362d9edf6e8d14ca9638dd9ce74071656f1d
Fulton Garcia <fultongarcia@gmail.com>
2020-03-18 18:17:44 -0400

    Updates

---
+++ precache-manifest.313852bc71c90a1a90e6bf00c0eac74a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b02dc6fc64287de9f111bee6d3db58d",
+    "url": "/Transit-Alliance-Frontend/index.html"
+  },
+  {
+    "revision": "fee497bf810f609c0c0c",
+    "url": "/Transit-Alliance-Frontend/static/css/2.cadd62b9.chunk.css"
+  },
+  {
+    "revision": "f16431f46eb61c75e3aa",
+    "url": "/Transit-Alliance-Frontend/static/css/main.7f435a0c.chunk.css"
+  },
+  {
+    "revision": "fee497bf810f609c0c0c",
+    "url": "/Transit-Alliance-Frontend/static/js/2.fd4e56ae.chunk.js"
+  },
+  {
+    "revision": "14576e628ce8fe44573c43763ba1508d",
+    "url": "/Transit-Alliance-Frontend/static/js/2.fd4e56ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16431f46eb61c75e3aa",
+    "url": "/Transit-Alliance-Frontend/static/js/main.36e82367.chunk.js"
+  },
+  {
+    "revision": "cc6539a1f24c15337d2f",
+    "url": "/Transit-Alliance-Frontend/static/js/runtime-main.32872320.js"
+  },
+  {
+    "revision": "81539b7e8466571dcc9a7c786c65fcc1",
+    "url": "/Transit-Alliance-Frontend/static/media/bus-1.81539b7e.jpg"
+  },
+  {
+    "revision": "1de8e0118406f4384f6fdc4aa617da00",
+    "url": "/Transit-Alliance-Frontend/static/media/bus-2.1de8e011.jpg"
+  },
+  {
+    "revision": "c50e6d470ab979f5f94bb0afe4542a4c",
+    "url": "/Transit-Alliance-Frontend/static/media/bus-3.c50e6d47.jpg"
+  },
+  {
+    "revision": "d7547632c11dec6939c866d23dc2ebbd",
+    "url": "/Transit-Alliance-Frontend/static/media/bus-a.d7547632.jpg"
+  },
+  {
+    "revision": "7b742d628be7d7a43966ca7f795897c0",
+    "url": "/Transit-Alliance-Frontend/static/media/bus-b.7b742d62.jpg"
+  },
+  {
+    "revision": "dbe4d4ca9a69029f6453d2e5913a8f9a",
+    "url": "/Transit-Alliance-Frontend/static/media/bus-c.dbe4d4ca.jpg"
+  },
+  {
+    "revision": "b20db39aa1a156a8e37c6688ec693439",
+    "url": "/Transit-Alliance-Frontend/static/media/metromover-station.b20db39a.jpg"
+  },
+  {
+    "revision": "619513c4e00281248a42828e14b67b41",
+    "url": "/Transit-Alliance-Frontend/static/media/mlogo.619513c4.png"
+  },
+  {
+    "revision": "ab3c28d9f167db97e8214c2d47aef251",
+    "url": "/Transit-Alliance-Frontend/static/media/public-works.ab3c28d9.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Transit-Alliance-Frontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Transit-Alliance-Frontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Transit-Alliance-Frontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Transit-Alliance-Frontend/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "db964d37aafd406d0a6d1508c8bce64c",
+    "url": "/Transit-Alliance-Frontend/static/media/sts.db964d37.jpg"
+  }
+]);
