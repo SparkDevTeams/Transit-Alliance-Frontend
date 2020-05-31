94e4a46aeddb964a209b456f6089a1bfc28b7547
Fulton Garcia <fultongarcia@gmail.com>
2020-05-31 13:12:15 -0400

    Updates

---
+++ precache-manifest.43fcf85f404834dfa514a0ba022ddb00.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94436d7150f1af3a96e92615f9e4425f",
+    "url": "/Transit-Alliance-Frontend/index.html"
+  },
+  {
+    "revision": "2f793be9634a4c4efc34",
+    "url": "/Transit-Alliance-Frontend/static/css/2.def019a4.chunk.css"
+  },
+  {
+    "revision": "1275c021c360cdc25a95",
+    "url": "/Transit-Alliance-Frontend/static/css/main.ef4919c1.chunk.css"
+  },
+  {
+    "revision": "2f793be9634a4c4efc34",
+    "url": "/Transit-Alliance-Frontend/static/js/2.b453fe20.chunk.js"
+  },
+  {
+    "revision": "c252fe9cf1ed82d21a64fae0719cd036",
+    "url": "/Transit-Alliance-Frontend/static/js/2.b453fe20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1275c021c360cdc25a95",
+    "url": "/Transit-Alliance-Frontend/static/js/main.417ffb52.chunk.js"
+  },
+  {
+    "revision": "cc6539a1f24c15337d2f",
+    "url": "/Transit-Alliance-Frontend/static/js/runtime-main.32872320.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Transit-Alliance-Frontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Transit-Alliance-Frontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Transit-Alliance-Frontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Transit-Alliance-Frontend/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Transit-Alliance-Frontend/precache-manifest.b2dd5e0940f0f95ace5fbeb1d63c54f0.js"
+  "/Transit-Alliance-Frontend/precache-manifest.43fcf85f404834dfa514a0ba022ddb00.js"
 );
 
 self.addEventListener('message', (event) => {
